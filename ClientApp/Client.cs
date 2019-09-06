7e81f23fde90d42892e2670d7cbd1a9e1c0823b0
tee29909 <59160571@go.buu.ac.th>
2019-09-06 14:26:58 +0700

    first commit

---
+++ ClientApp/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = System.IO.File.ReadAllText(@"C:\Users\Master\Desktop\TCPIPAPP\a.txt");
+            
+            //Console.WriteLine(s);
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("192.168.0.13"), 8080);
+            sck.Connect(endpoint);
+            //Console.Write("Enter Message:");
+           // string msg = Console.ReadLine();
+            byte[] msgbuffer = Encoding.ASCII.GetBytes(s.ToLower());
+            sck.Send(msgbuffer, 0, msgbuffer.Length, 0);
+
+            byte[]  buffer = new byte[1024];
+            int rec = sck.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received :{0}", Encoding.Default.GetString(buffer));
+            sck.Close();
+     
+            Console.Read();
+        }
+    }
+}
---
+++ TCPIPAPP/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPIPAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            
+
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("192.168.0.1"), 1990);
+            sck.Bind(endpoint);
+            sck.Listen(10);
+            Socket acc = sck.Accept();
+            string message = "Hello";
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            acc.Send(buffer, 0, buffer.Length, 0);
+
+
+            buffer = new byte[1024];
+            Console.WriteLine(buffer);
+            int rec = acc.Receive(buffer, 0, buffer.Length, 0);
+            Array.Resize(ref buffer, rec);
+            Console.WriteLine("Received :{0}", Encoding.Default.GetString(buffer));
+            if (Encoding.Default.GetString(buffer) != "")
+            {
+                System.IO.File.WriteAllText(@"C:\Users\Master\Desktop\TCPIPAPP\a_small.txt", Encoding.Default.GetString(buffer).ToLower());
+            }
+            
+            sck.Close();
+            acc.Close();
+            Console.Read();
+        }
+    }
+}
